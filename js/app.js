a7d8eee3c2dbd086a1eef80148ae6cfb44ac743f
Mark Myers <markalope@gmail.com>
2018-05-10 15:11:25 -0700

    created locations array

--- js/app.js
+++ js/app.js
@@ -1,17 +1,2 @@
 'use strict';
 
-class Stores {
-
-    constructor(location, min, max, avg) {
-        this.location = location;
-        this.min = min;
-        this.max = max;
-        this.avg = avg;
-
-        this.random = function() {
-            var i = Math.floor(Math.random() * (max - min * 1)) + min;
-            return i * this.avg;
-        };
-    }
-
-}
